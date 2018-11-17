6d4d07640c75b0032f95547afa6423bc517c5ce1
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-11-17 15:42:12 -0200

    Refatorando o GetCenário e colocando as métricas na árvore splay

--- src/main/java/action/GetCenario1Action.java
+++ src/main/java/action/GetCenario1Action.java
@@ -1,6 +1,7 @@
 package action;
 
 import arvores.ArvoreAVL;
+import arvores.ArvoreSplay;
 import arvores.MinhaArvore;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -106,10 +107,89 @@ public class GetCenario1Action implements Action { // Implementar Action e é re
             case 1: { // Deseja ordenar por inteiros
                 break;
             }
-            case 2: { // Realiza o mesmo processo dos dois citados acima, contudo, a execução é uma seguida da outra, pois essa opção seria de ordenar os dois de uma só vez.
+            case 3: { // Realiza o mesmo processo dos dois citados acima, contudo, a execução é uma seguida da outra, pois essa opção seria de ordenar os dois de uma só vez.
                 break;
             }
-            case 3: {
+            case 2: {
+                Integer contador = 0;
+                Integer[] quantidadeLeitura = ArquivoDAO.getInstance(); // Recebe a quantidade de dados que deverá ser lida
+                for (int i = 1; i < quantidadeLeitura.length; i++) { // Executa 6 vezes, pois são 6 números
+                    Resultado resultados[] = new Resultado[25];
+                    for (int j = 0; j < 5; j++) { // Executa as 5 sementes
+
+                        GastoDAO.shuffle(); // Embaralha o ArrayList
+                        Gasto[] analise = new Gasto[quantidadeLeitura[i]]; // Cria um vetor com o tamanho necessário definido no Entrada.txt ou informado pelo usuário
+                        for (int k = 0; k < quantidadeLeitura[i]; k++) { // Lê a quantidade desejada do ArrayList
+                            analise[k] = GastoDAO.getInstance().get(k); // O vetor recebe do ArrayList os elementos para serem ordenados
+                        }
+                        ArvoreSplay splay = new ArvoreSplay();
+                        resultados[j] = new Resultado();
+                        splay.insereGastos(analise, resultados[j]);
+
+                        GastoDAO.shuffle();
+                        Gasto[] analise2 = new Gasto[quantidadeLeitura[i]];
+                        for (int k = 0; k < quantidadeLeitura[i]; k++) { // Lê a quantidade desejada do ArrayList
+                            analise2[k] = GastoDAO.getInstance().get(k); // O vetor recebe do ArrayList os elementos para serem ordenados
+                        }
+                        resultados[j + 5] = new Resultado();
+                        splay.buscaGastos(analise2, resultados[j + 5]);
+
+                        Integer contadorAux = 0;
+                        for (int k = (int) (analise.length * 0.7); k < quantidadeLeitura[i]; k++) {
+                            analise2[k] = analise2[contadorAux];
+                            contadorAux++;
+                        }
+                        QuickSort quick = new QuickSort();
+                        Resultado resultado = new Resultado();
+                        quick.ordenaObjeto(analise2, resultado);
+                        resultados[j + 10] = new Resultado();
+                        splay.buscaGastos(analise2, resultados[j + 10]);
+
+                        ArvoreSplay splay2 = new ArvoreSplay();
+                        try {
+                            splay2 = splay.clone();
+                        } catch (CloneNotSupportedException ex) {
+                            Logger.getLogger(GetCenario1Action.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        resultados[j + 15] = new Resultado();
+                        splay2.excluirGastos(analise2, resultados[j + 15]);
+
+                        for (int k = (int) (analise.length * 0.7); k < quantidadeLeitura[i]; k++) {
+                            analise[k] = analise[contadorAux];
+                            contadorAux++;
+                        }
+                        quick.ordenaObjeto(analise, resultado);
+                        resultados[j + 20] = new Resultado();
+                        splay.excluirGastos(analise, resultados[j + 20]);
+                        contador++;
+                    }
+                    for (int k = 0; k < 25; k = k + 5) {
+                        Resultado resultad[] = new Resultado[5];
+                        resultad[0] = new Resultado();
+                        resultad[0] = resultados[k];
+                        resultad[1] = new Resultado();
+                        resultad[1] = resultados[k + 1];
+                        resultad[2] = new Resultado();
+                        resultad[2] = resultados[k + 1];
+                        resultad[3] = new Resultado();
+                        resultad[3] = resultados[k + 1];
+                        resultad[4] = new Resultado();
+                        resultad[4] = resultados[k + 1];
+                        if (k == 0) {
+                            ArquivoDAO.escrever(0, (contador - 5) + "Arvore Splay - Inserção", resultad);
+                        } else if (k == 5) {
+                            ArquivoDAO.escrever(0, (contador - 5) + "Arvore Splay - Busca 1", resultad);
+                        } else if (k == 10) {
+                            ArquivoDAO.escrever(0, (contador - 5) + "Arvore Splay - Busca 2", resultad);
+                        } else if (k == 15) {
+                            ArquivoDAO.escrever(0, (contador - 5) + "Arvore Splay - Remoção 1", resultad);
+                        } else if (k == 20) {
+                            ArquivoDAO.escrever(0, (contador - 5) + "Arvore Splay - Remoção 2", resultad);
+                        }
+                    }
+                }
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/resultadosOrdenacao.jsp"); // Chama o jsp para mostrar o resultado
+                dispatcher.forward(request, response);
                 break;
             }
             case 4: {
--- src/main/java/arvores/ArvoreSplay.java
+++ src/main/java/arvores/ArvoreSplay.java
@@ -1,10 +1,40 @@
 package arvores;
 
 import java.util.Objects;
+import model.Gasto;
+import model.Resultado;
 
-public class ArvoreSplay {
+public class ArvoreSplay implements Cloneable {
 
+    private Resultado resultado;
     private NoArvoreBinariaSplay raiz;
+
+    public void insereGastos(Gasto vetor[], Resultado resultado) {
+        this.resultado = resultado;
+        long tempoInicial = System.nanoTime();
+        for (int i = 0; i < vetor.length; i++) {
+            inserirSplay(vetor[i].getIdGasto());
+        }
+        resultado.setTempoGasto(System.nanoTime() - tempoInicial);
+    }
+
+    public void buscaGastos(Gasto vetor[], Resultado resultado) {
+        this.resultado = resultado;
+        long tempoInicial = System.nanoTime();
+        for (int i = 0; i < vetor.length; i++) {
+            procurar(vetor[i].getIdGasto());
+        }
+        resultado.setTempoGasto((System.nanoTime() - tempoInicial));
+    }
+
+    public void excluirGastos(Gasto vetor[], Resultado resultado) {
+        this.resultado = resultado;
+        long tempoInicial = System.nanoTime();
+        for (int i = 0; i < vetor.length; i++) {
+            removerDaArvore(vetor[i].getIdGasto());
+        }
+        resultado.setTempoGasto((System.nanoTime() - tempoInicial));
+    }
 
     public ArvoreSplay() {
         this.raiz = null;
@@ -21,9 +51,13 @@ public class ArvoreSplay {
             }
 
             if (idGasto < no.getFilhoEsquerda().getIdGasto()) {
+                resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
+                resultado.setNumTrocas(resultado.getNumTrocas() + 1);
                 no.getFilhoEsquerda().setFilhoEsquerda(puxarParaRaiz(no.getFilhoEsquerda().getFilhoEsquerda(), idGasto));
                 no = rotacaoDireita(no);
             } else if (idGasto > no.getFilhoEsquerda().getIdGasto()) {
+                resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
+                resultado.setNumTrocas(resultado.getNumTrocas() + 1);
                 no.getFilhoEsquerda().setFilhoDireita(puxarParaRaiz(no.getFilhoEsquerda().getFilhoDireita(), idGasto));
                 if (no.getFilhoEsquerda().getFilhoDireita() != null) {
                     no.setFilhoEsquerda(rotacaoEsquerda(no.getFilhoEsquerda()));
@@ -41,11 +75,16 @@ public class ArvoreSplay {
             }
 
             if (idGasto < no.getFilhoDireita().getIdGasto()) {
+                resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
+                resultado.setNumTrocas(resultado.getNumTrocas() + 1);
                 no.getFilhoDireita().setFilhoEsquerda(puxarParaRaiz(no.getFilhoDireita().getFilhoEsquerda(), idGasto));
                 if (no.getFilhoDireita().getFilhoEsquerda() != null) {
+                    resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
                     no.setFilhoDireita(rotacaoDireita(no.getFilhoDireita()));
                 }
             } else if (idGasto > no.getFilhoDireita().getIdGasto()) {
+                resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
+                resultado.setNumTrocas(resultado.getNumTrocas() + 1);
                 no.getFilhoDireita().setFilhoDireita(puxarParaRaiz(no.getFilhoDireita().getFilhoDireita(), idGasto));
                 no = rotacaoEsquerda(no);
             }
@@ -71,17 +110,21 @@ public class ArvoreSplay {
         raiz = noAux;
 
         if (idGasto < raiz.getIdGasto()) {
+            resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
             NoArvoreBinariaSplay noAux2 = new NoArvoreBinariaSplay(idGasto);
             noAux2.setFilhoDireita(raiz);
             noAux2.setFilhoEsquerda(raiz.getFilhoEsquerda());
             raiz.setFilhoEsquerda(null);
             raiz = noAux2;
+            resultado.setNumTrocas(resultado.getNumTrocas() + 4);
         } else if (idGasto > raiz.getIdGasto()) {
+            resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
             NoArvoreBinariaSplay noAux2 = new NoArvoreBinariaSplay(idGasto);
             noAux2.setFilhoEsquerda(raiz);
             noAux2.setFilhoDireita(raiz.getFilhoDireita());
             raiz.setFilhoDireita(null);
             raiz = noAux2;
+            resultado.setNumTrocas(resultado.getNumTrocas() + 4);
         } else {
             raiz.setIdGasto(idGasto);
         }
@@ -103,6 +146,7 @@ public class ArvoreSplay {
                 raiz = raiz.getFilhoEsquerda();
                 puxarParaRaiz(noAux, idGasto);
                 raiz.setFilhoDireita(noAux);
+                resultado.setNumTrocas(resultado.getNumTrocas() + 1);
             }
         }
     }
@@ -110,6 +154,7 @@ public class ArvoreSplay {
     public Boolean procurar(Integer idGasto) {
         raiz = puxarParaRaiz(raiz, idGasto);
         if (Objects.equals(idGasto, raiz.getIdGasto())) {
+            resultado.setNumComparacoes(resultado.getNumComparacoes() + 1);
             return true;
         } else {
             return false;
@@ -120,6 +165,7 @@ public class ArvoreSplay {
         NoArvoreBinariaSplay noAux = no.getFilhoEsquerda();
         no.setFilhoEsquerda(noAux.getFilhoDireita());
         noAux.setFilhoDireita(no);
+        resultado.setNumTrocas(resultado.getNumTrocas() + 2);
         return noAux;
     }
 
@@ -127,6 +173,7 @@ public class ArvoreSplay {
         NoArvoreBinariaSplay noAux = no.getFilhoDireita();
         no.setFilhoDireita(noAux.getFilhoEsquerda());
         noAux.setFilhoEsquerda(no);
+        resultado.setNumTrocas(resultado.getNumTrocas() + 2);
         return noAux;
     }
 
@@ -138,5 +185,8 @@ public class ArvoreSplay {
         this.raiz = raiz;
     }
 
+    @Override
+    public ArvoreSplay clone() throws CloneNotSupportedException {
+        return (ArvoreSplay) super.clone(); //To change body of generated methods, choose Tools | Templates.
+    }
 }
-
